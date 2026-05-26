14582a3810f50aab6ebdecc1ebcec5c7232b1a8b
student-eng <student-eng@EDU-WSB.local>
2026-05-26 13:16:46 +0200

    '1'

--- day2/5_weight_height.py
+++ day2/5_weight_height.py
@@ -14,7 +14,14 @@ print(df.describe().T.to_string())
 plt.hist(df.query("Gender=='Female'")['Weight'], bins=30)
 plt.hist(df.query("Gender=='Male'")['Weight'], bins=30)
 plt.show()
-
+plt.scatter(df['Weight'],df['Height'])
+plt.show()
+print('How many men and how many women')
+print(df['Gender'].value_counts())
+print('New scatter for men and women')
+plt.scatter(df.query("Gender=='Female'")['Weight'] , df.query("Gender=='Female'")['Height'])
+plt.scatter(df.query("Gender=='Male'")['Weight'] , df.query("Gender=='Male'")['Height'])
+plt.show()
 
 
 
