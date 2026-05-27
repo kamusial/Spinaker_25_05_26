4d13ef66d84aefb1d242f04cbfc8ea4bb4ecaebb
student-eng <student-eng@EDU-WSB.local>
2026-05-27 11:48:53 +0200

    '1'

--- day3/2_diabetes_logistic_regression.py
+++ day3/2_diabetes_logistic_regression.py
@@ -9,3 +9,21 @@ print(df.describe().T.to_string())
 print(f'\nHow many missing values is columns:\n{df.isna().sum()}')
 
 # everywhere, where 0 and N/A put average for column
+for col in ['glucose', 'bloodpressure', 'skinthickness', 'insulin',
+       'bmi', 'diabetespedigreefunction', 'age']:
+    df[col] = df[col].replace(0, np.nan)
+    mean_ = df[col].mean()
+    df[col] = df[col].replace(np.nan, mean_)
+
+print(f'\nAfter fixing:\nHow many missing values is columns:\n{df.isna().sum()}')
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+
+X =      # inputs
+y =      # outputs
+#tran test split
+
+model = LogisticRegression()
+# fit the model, show results
